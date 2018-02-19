80cd43aadee84516e71ec1e4cacedd5fddd112eb
Manuel <manuel.delverme@gmail.com>
2018-02-19 18:45:03 +0100

    using folders to speed up cache get

--- disk_utils.py
+++ disk_utils.py
@@ -17,16 +17,19 @@ def disk_cache(function):
         fid = function.__name__
         cache_file = "cache/{}".format(fid)
         if args:
-            if not os.path.exists(cache_file):
-                os.makedirs(cache_file)
             args_filtered = []
             for arg in args:
                 arg = str(arg)
                 if len(arg) > 100:
                     arg = hashlib.sha1(arg.encode('utf-8')).hexdigest()
-                args_filtered.append(arg)
+                args_filtered.append(arg.replace("/", "_"))
+            if function.__name__ == "eval_option_on_mdp":
+                option_hash = args_filtered[-2]
+                del args_filtered[-2]
+                new_hash = list(option_hash[:6]) + [option_hash[6:]]
+                args_filtered = args_filtered[:-1] + new_hash + args_filtered[-1:]
 
-            fid = fid + "/" + "::".join(args_filtered).replace("/", "_")
+            fid = fid + "/" + "/".join(args_filtered)
             cache_file = "cache/{}".format(fid)
         cache_file += ".pkl.gz"
 
@@ -34,20 +37,12 @@ def disk_cache(function):
             with gzip.open(cache_file, "rb") as fin:
                 retr = pickle.load(fin)
         except FileNotFoundError:
-            try:
-                # OLD STYLE
-                with open(cache_file[:-3], "rb") as fin:
-                    retr = pickle.load(fin)
-            except (FileNotFoundError, OSError):
-                retr = function(*args, **kwargs)
-
+            retr = function(*args, **kwargs)
+            if not os.path.exists(cache_file):
+                cache_folder = cache_file[:cache_file.rindex("/")]
+                os.makedirs(cache_folder, exist_ok=True)
             with gzip.open(cache_file, "wb") as fout:
                 pickle.dump(retr, fout)
-
-            try:
-                os.remove(cache_file[:-3])
-                print("removed old file", cache_file[:-3])
-            except Exception:
-                pass
         return retr
+
     return wrapper
