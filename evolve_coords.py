92b22a4382c13efc9f16262b229bd3be52c55585
pc1 <pc1@cnr>
2018-02-21 15:32:37 +0100

    standardizing eval options parameters

--- evolve_coords.py
+++ evolve_coords.py
@@ -2,6 +2,7 @@ import matplotlib
 
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import envs.simple_boxes
 import learners
 import controller.meta_controller
 import itertools
@@ -11,60 +12,43 @@ import envs.simple_boxes as e
 import options_utils
 
 
-def gather_sensor_readings(world_tiles, world_walls, world_width):
-    sensor_readings = [None, ] * world_tiles
-
-    for position_idx in range(world_tiles):
-        if position_idx in world_walls:
-            continue
-        # tmp_world.show_board(highlight_square=position_idx)
-        can_reach = np.ones(shape=(3, 3), dtype=np.bool) * False
-
-        can_reach[1][1] = True
-
-        x0 = position_idx % world_width
-        y0 = position_idx // world_width
-        for y_shift in (-1, 0, 1):
-            for x_shift in (-1, 0, 1):
-                x = x0 + x_shift
-                y = y0 + y_shift
-                pos_shift = position_idx + x_shift + y_shift * world_width
-                # print("\n{},{} ({}) -> {},{} ({})".format(x0, y0, position_idx, x, y, pos_shift), end='')
-                if x < 0 or x >= world_width:
-                    continue
-                if y < 0 or y >= world_width:
-                    continue
-                if pos_shift in world_walls:
-                    continue
-                # print(" can_reach", end='')
-                can_reach[y_shift + 1, x_shift + 1] = True
-
-        sensor_readings[position_idx] = can_reach.flatten()
-    return sensor_readings
-
-
 def fitness_simple_boxes(args):
     coords_vector, SIDE_SIZE, nr_options, possible_box_positions, budget, plot_progress = args
     assert budget % 10 == 0
-    xs = [10 + 10 * x for x in range(10000)]
 
-    options = []
+    token_mdp = envs.simple_boxes.BoxWorldSimple(side_size=SIDE_SIZE)
+
+    goal_idxs = set()
     for x, y in list(zip(coords_vector, coords_vector[1:]))[::2]:
         x = max(0, x)
         y = max(0, y)
+        x = min(SIDE_SIZE, x)
+        y = min(SIDE_SIZE, y)
         goal_idx = int(x) * SIDE_SIZE + int(y) * SIDE_SIZE * SIDE_SIZE
-        option = tuple(learners.q_learning.learn_option(goal_idx, e.BoxWorldSimple(side_size=SIDE_SIZE)))
-        options.append(option)
+        goal_idxs.add(goal_idx)
 
-    # eval options
+    # possible_tiles = [position_idx for position_idx in range(token_mdp.number_of_tiles) if position_idx not in token_mdp._walls]
+    # TODO: check walls; if walls: return -inf
+    import ipdb; ipdb.set_trace()
+    option_set = list(sorted(goal_idxs))
+
+    xs = [10 + 10 * x for x in range(1000)]
+    possible_box_positions = list(itertools.combinations([0, SIDE_SIZE - 1, (SIDE_SIZE * SIDE_SIZE) - SIDE_SIZE, SIDE_SIZE * SIDE_SIZE - 1, ], 2))
+    learner = learners.q_learning.QLearning(env=token_mdp, options=[])
+
+    option_sets_scores = {}
+    options = [options_utils.goal_to_policy(learner, goal_idx, token_mdp) for goal_idx in option_set]
+    # assert len(options) == 4
+    assert list(sorted(option_set)) == option_set
     fitnesses = options_utils.eval_options(SIDE_SIZE, options, possible_box_positions, xs)
-    option_names = options_utils.name_options(options)
 
+    option_names = option_set
     if plot_progress:
         option_names = " ".join(str(n) for n in sorted(option_names))
         plt.plot(xs, fitnesses)
         plt.title(option_names)
         plt.show()
+    print("opt-names:", " ".join(str(n) for n in sorted(option_names)), "fitness:", fitnesses[int(budget / 10)])
     return fitnesses[int(budget / 10)]
 
 
